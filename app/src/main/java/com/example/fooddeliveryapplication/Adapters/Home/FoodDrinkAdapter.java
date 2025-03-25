494bbb4d46770fe479d89f8dfad2a59a7fdec75e
Le Phuc <phuc.le@savvycom.vn>
2025-03-25 20:52:39 +0700

    Home & Login

---
+++ app/src/main/java/com/example/fooddeliveryapplication/Adapters/Home/FoodDrinkAdapter.java
@@ -0,0 +1,28 @@
+package com.example.fooddeliveryapplication.Adapters.Home;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FoodDrinkAdapter extends FragmentStateAdapter {
+    private final String userId;
+
+    public FoodDrinkAdapter(@NonNull Fragment fragment, String id) {
+        super(fragment);
+        userId = id;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 1) {
+            return new DrinkHomeFrg(userId);
+        }
+        return new FoodHomeFrg(userId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
