a32c6c5e3991ece59545aa5b41a2f1808ca1869e
Linh Nguyen Thao <linh.nguyn80990@gmail.com>
2025-04-27 23:06:24 +0700

    config: firebase config

--- app/src/main/java/com/example/app/Fragments/NotificationFragment.java
+++ app/src/main/java/com/example/app/Fragments/NotificationFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.app.Helpers.FirebaseNotificationHelper;
 import com.example.app.databinding.FragmentNotificationBinding;
 
 import java.util.List;
